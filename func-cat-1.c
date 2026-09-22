9e1120b33b0593785cbd7855f583eac915ee2e13
CHARITHA REDDY DUMPA <cherrycharitha2008@gmail.com>
2026-09-22 12:55:42 +0530

    Add files via upload

---
+++ func-cat-1.c
@@ -0,0 +1,16 @@
+//WAP to display addition of 2 numbers using functions
+//cat-1
+#include <stdio.h>
+
+    void add();
+    int main()
+    {
+        add();
+        return 0;
+    }
+    void add()
+    {
+        int a = 10, b = 20, c;
+        c=a+b;
+        printf("addition is=%d", c);
+    }
